f5ada076b4d7871b75a4e848d1bab1f279990b7b
hyeonmin.song <hyeonmin.song@techwing.co.kr>
2021-07-07 23:49:28 +0900

    0706

--- 1463_1로만들기_DP.cpp
+++ 1463_1로만들기_DP.cpp
@@ -2,22 +2,32 @@
 #include <algorithm>
 using namespace std;
 
-int dp[1000001];
-
-int solve(int N)
+int dp1[1000001]; //최소횟수 저장할 dp배열 
+int dp2[1000001];
+//top-down, recursion 
+int solve1(int N)
 {
-	int cnt;
-	
+
 	if ( N == 1) return 0;
-	if ( dp[N] > 0) return dp[N];
+	if ( dp1[N] > 0) return dp1[N];
 	
-	cnt = solve(N - 1) + 1;
-	if ( N % 3 == 0) cnt = min(solve(N/3)+ 1, cnt);
-	if ( N % 2 == 0) cnt = min(solve(N/2) +1, cnt);
+	dp1[N] = solve1(N - 1) + 1;
+	if ( N % 3 == 0) dp1[N] = min(solve1(N/3)+1, dp1[N]);
+	if ( N % 2 == 0) dp1[N] = min(solve1(N/2)+1, dp1[N]);
 	
-	dp[N] = cnt;
+	return dp1[N];
+}
+//bottom-up
+void solve2(int N)
+{
+	dp2[1] = 0;
 	
-	return cnt;
+	for (int i = 2; i <= N; i++)
+	{
+		dp2[i] = dp2[i-1] + 1;
+		if (i%3 == 0) dp2[i] = min(dp2[i/3]+1, dp2[i]);
+		if (i%2 == 0) dp2[i] = min(dp2[i/2]+1, dp2[i]);
+	}
 }
 
 
@@ -26,8 +36,10 @@ int main(void)
 	int N;
 	scanf("%d",&N);
 	
-	solve(N);
+	solve1(N);
+	solve2(N);
 	
-	printf("%d\n", dp[N]);
+	printf("%d %d\n", dp1[N], dp2[N]);
+
 	return 0;
 }
